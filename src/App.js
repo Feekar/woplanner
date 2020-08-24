a6246a1fdb3761daec2a68833f8ad63369097217
Jonas Gustavsson <jonasgson@outlook.com>
2020-08-24 13:35:10 +0200

    friebase and .env

--- src/App.js
+++ src/App.js
@@ -3,12 +3,51 @@ import { Toolbar, Weeks } from "./components";
 
 import "./App.css";
 
+function Week(weekNumber = "", days = []) {
+  this.weekNumber = weekNumber;
+  this.days = days;
+}
+
+function Day(name = "", exercises = []) {
+  this.name = name;
+  this.exercises = exercises;
+}
+
 function App() {
   const [weeksRepeated, setWeeksRepeated] = useState(1);
+
+  console.log(process.env.REACT_APP_API_KEY);
 
   const handleUpdateWeeksRepeated = (weeks) => {
     setWeeksRepeated(weeks);
   };
+
+  //   const workout = {
+  //     weeks: [
+  //       {
+  //         weekNumber: 1,
+  //         days: [
+  //           {
+  //             name: "MONDAY",
+  //             exercises: [
+  //               {
+  //                 name: "",
+  //                 type: "strength"
+  //               }
+  //             ]
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         weekNumber: 2,
+  //         days: []
+  //       },
+  //       {
+  //         weekNumber: 3,
+  //         days: []
+  //       },
+  //     ]
+  // }
 
   return (
     <div className="App">
