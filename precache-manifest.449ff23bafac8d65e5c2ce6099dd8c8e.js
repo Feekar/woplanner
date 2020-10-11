811dc0ee15d2e7fa6880df46ee96321b4b1608cb
Jonas Gustavsson <jonasgson@outlook.com>
2020-10-11 22:52:03 +0200

    Updates

---
+++ precache-manifest.449ff23bafac8d65e5c2ce6099dd8c8e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c9ec003ebcbb57d5d13395fcea5d7a",
+    "url": "/woplanner/index.html"
+  },
+  {
+    "revision": "5344224631cfc058e0e6",
+    "url": "/woplanner/static/css/main.85af2206.chunk.css"
+  },
+  {
+    "revision": "96b4a0aad65fa5f7041d",
+    "url": "/woplanner/static/js/2.a4c37f7d.chunk.js"
+  },
+  {
+    "revision": "2d4bd69c93f2f96c5b55b57aad25f42d",
+    "url": "/woplanner/static/js/2.a4c37f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5344224631cfc058e0e6",
+    "url": "/woplanner/static/js/main.1c4c5b3f.chunk.js"
+  },
+  {
+    "revision": "3594fc8bb2e4812536d2",
+    "url": "/woplanner/static/js/runtime-main.bb8b3fe6.js"
+  },
+  {
+    "revision": "8c6b44053f02db2b58030731310bd693",
+    "url": "/woplanner/static/media/add_note.8c6b4405.svg"
+  },
+  {
+    "revision": "f1de5f666d04a0ec01fb8c02e106fa1e",
+    "url": "/woplanner/static/media/cardio.f1de5f66.svg"
+  },
+  {
+    "revision": "85de99cb610e62ba0857e499b45decec",
+    "url": "/woplanner/static/media/flexibility.85de99cb.svg"
+  },
+  {
+    "revision": "9661ba0b1e7abb32b6b4b956d5d14899",
+    "url": "/woplanner/static/media/minus.9661ba0b.svg"
+  },
+  {
+    "revision": "80f0d9c3b3639770eef2693f5c84577b",
+    "url": "/woplanner/static/media/plus.80f0d9c3.svg"
+  },
+  {
+    "revision": "9a48fe3d6099b1d87873fe06dda3ebbe",
+    "url": "/woplanner/static/media/sport.9a48fe3d.svg"
+  },
+  {
+    "revision": "9e3c11ef10514be71100781b27f74f16",
+    "url": "/woplanner/static/media/strength.9e3c11ef.svg"
+  },
+  {
+    "revision": "4fd288544537fc5dd204cf0d68fda96b",
+    "url": "/woplanner/static/media/trash.4fd28854.svg"
+  }
+]);
